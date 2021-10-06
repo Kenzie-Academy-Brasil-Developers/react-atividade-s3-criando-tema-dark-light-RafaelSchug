80f114c755d5a00afbf9d387361f7c5a491d4f5a
Rafael Schug <rafaelschugbc@gmail.com>
2021-10-06 19:47:46 -0300

    add: dark/light themes

---
+++ src/themes.js
@@ -0,0 +1,25 @@
+const lightTheme = {
+    color: "#0e14e",
+    backgroundColor: "#D3D4E6",
+    button: {
+      textColor: "#D3D4E6",
+      borderColor: "#151A69",
+      backgroundColor: "#151A69",
+    },
+  };
+  
+  const darkTheme = {
+    color: "#ffffff",
+    backgroundColor: "#2a2d31",
+    button: {
+      textColor: "#fff",
+      borderColor: "#151A69",
+      backgroundColor: "#000000",
+    },
+  };
+  
+  export const themes = {
+    light: lightTheme,
+    dark: darkTheme,
+  };
+  
